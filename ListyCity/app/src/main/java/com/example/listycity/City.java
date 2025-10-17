49cd31798a2f86847d7ca5c31ff5ac35e9825003
Louis Lam <leelaw@ualberta.ca>
2025-10-17 15:22:01 -0600

    time to play silk song (nvm i got group project)

--- ListyCity/app/src/main/java/com/example/listycity/City.java
+++ ListyCity/app/src/main/java/com/example/listycity/City.java
@@ -1,4 +1,46 @@
 package com.example.listycity;
 
-public class City {
+/**
+ * This is a class that defines a City.
+ */
+public class City implements Comparable<City> {
+    private String city;
+    private String province;
+    City(String city, String province){
+        this.city = city;
+        this.province = province;
+    }
+    String getCityName(){
+        return this.city;
+    }
+    String getProvinceName(){
+        return this.province;
+    }
+    @Override
+    public int compareTo(City o) {
+        City city = (City) o;
+        return this.city.compareTo(city.getCityName()); // this.city refers to the city name
+    }
+
+    /** Cities are equal if both name and province match (case-sensitive). */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof City)) return false;
+        City other = (City) obj;
+        return this.city.equals(other.city) && this.province.equals(other.province);
+    }
+
+    /** Must be consistent with equals for collections to behave correctly. */
+    @Override
+    public int hashCode() {
+        int result = city.hashCode();
+        result = 31 * result + province.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return city + ", " + province;
+    }
 }
--- ListyCity/app/src/main/java/com/example/listycity/CityList.java
+++ ListyCity/app/src/main/java/com/example/listycity/CityList.java
@@ -1,4 +1,59 @@
 package com.example.listycity;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * This is a class that keeps a list of city objects
+ */
 public class CityList {
+    private List<City> cities = new ArrayList<>();
+
+    /**
+     * This adds a city to the list if the city does not exist
+     * @param city
+     *      This is a candidate city to add
+     * @throws IllegalArgumentException if the city already exist
+     */
+    public void add(City city) {
+        if (cities.contains(city)) {
+            throw new IllegalArgumentException();
+        }
+        cities.add(city);
+    }
+    /**
+     * This returns a sorted list of cities
+     * @return
+     * Return the sorted list
+     */
+    public List<City> getCities() {
+        List<City> list = cities;
+        Collections.sort(list);
+        return list;
+    }
+    /**
+     * Returns true if the given city exists in the list.
+     */
+    public boolean hasCity(City city) {
+        return cities.contains(city);
+    }
+
+    /**
+     * Removes the given city if present; otherwise throws an exception.
+     * @throws IllegalArgumentException if the city is not found
+     */
+    public void delete(City city) {
+        boolean removed = cities.remove(city);
+        if (!removed) {
+            throw new IllegalArgumentException("City not found: " + city);
+        }
+    }
+
+    /**
+     * @return the number of cities currently in the list
+     */
+    public int countCities() {
+        return cities.size();
+    }
 }
